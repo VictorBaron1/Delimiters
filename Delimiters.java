6b2d661d4ea09bc1a815610130e3ba37b2b5a2c0
VictorBaron1 <victorb104@nycstudents.net>
2025-05-01 13:16:10 +0000

    final

--- Delimiters.java
+++ Delimiters.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Delimiters
 {
 /** The open and close delimiters. */
@@ -9,16 +10,36 @@ private String closeDel;
 */
 public Delimiters(String open, String close)
 {
-openDel = open;
-closeDel = close;
+    openDel = open;
+    closeDel = close;
 }
 /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
 public ArrayList<String> getDelimitersList(String[] tokens)
-{ /* to be implemented in part (a) */ }
+{
+    ArrayList<String> delimiters = new ArrayList<String>();
+    for(String s : tokens)
+    {
+        if(s.equals(openDel) || s.equals(closeDel))
+        {
+            delimiters.add(s);
+        }
+    }
+    return delimiters;
+}
 /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
 * Precondition: delimiters contains only valid open and close delimiters.
 */
 public boolean isBalanced(ArrayList<String> delimiters)
-{ /* to be implemented in part (b) */ }
+{
+    int opens = 0;
+    int closes = 0;
+    for(String s : delimiters)
+    {
+        if(s.equals(openDel)) opens++;
+        if(s.equals(closeDel)) closes++;
+        if(closes > opens) return false;
+    }
+    return opens == closes;
+}
 // There may be instance variables, constructors, and methods that are not shown.
 }
